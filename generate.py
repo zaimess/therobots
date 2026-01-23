8ee1b24e8c2d79667c904f69721562f0f17b786b
Zach Haimes <zach007attack@gmail.com>
2026-01-22 22:13:14 -0500

    Added boxes to world, 25 stacked columns

--- generate.py
+++ generate.py
@@ -1,6 +1,20 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+pyrosim.Start_SDF("boxs.sdf")
+# pyrosim.Send_Cube(name="Box", pos=[0,0,1.5] , size=[1,2,3])
+# pyrosim.Send_Cube(name="Box2", pos=[0,0,.5] , size=[1,1,1])
+
+
+def oneblocktower(row, col):
+    for i in range(10):
+        z = i
+        s = .9 ** i
+        pyrosim.Send_Cube(name="Box", pos=[row, col, .5 + z], size=[s, s, s])
+
+
+for p in range(5):
+    for j in range(5):
+        oneblocktower(j, p)
+
 pyrosim.End()
 
--- simulate.py
+++ simulate.py
@@ -1,8 +1,13 @@
 import pybullet as p
+import pybullet_data
 import time
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
+p.loadSDF("boxs.sdf")
 for i in range(1000):
     p.stepSimulation()
     time.sleep(1/60)
